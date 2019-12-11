046d7952dbcba1430a78f07fc6e5d0d7d0e062b3
Oliver Lauridsen <oliverlauridsen@Olivers-MacBook-Pro.local>
2019-12-11 10:42:00 +0100

    Updates all npm packages to the newest versions

--- webpack.common.js
+++ webpack.common.js
@@ -2,56 +2,59 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	devtool: 'inline-source-map',
-	output: {
-		path: path.resolve("dist", "assets"),
-		filename: "bundle.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-			{
-				test: /\.(s*)css$/,
-				use: [
-					{
-						loader: "style-loader", // Creates style nodes from JS strings.
-					},
-					{
-						loader: "css-loader", // Translates CSS into CommonJS.
-						options: {
-							sourceMap: true,
-						},
-					},
-					{
-						loader: "sass-loader", // Compiles Sass to CSS.
-						options: {
-							includePaths: [
-								"./src/styles/common", // Allows for import from this folder without specifying path.
-							],
-							sourceMap: true,
-						},
-					},
-				],
-			},
-			{
-				test: /\.svg$/,
-				loader: 'svg-inline-loader',
-			},
-		],
-	},
-	externals: {
-        zendesk_app_framework_sdk: 'ZAFClient',
-    },
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "src/index.html",
-			hash: true,
-		}),
-	],
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve("dist", "assets"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: "style-loader", // Creates style nodes from JS strings.
+          },
+          {
+            loader: "css-loader", // Translates CSS into CommonJS.
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader", // Compiles Sass to CSS.
+            options: {
+              sassOptions: {
+                includePaths: [
+                  "./src/styles/common", // Allows for import from this folder without specifying path.
+                ],
+              },
+
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+    ],
+  },
+  externals: {
+    zendesk_app_framework_sdk: "ZAFClient",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      hash: true,
+    }),
+  ],
 };
