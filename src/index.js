92d880421e64b51c47e3bac4cb1bbc6c08a625f1
Jens Ahlsten Herlevsen <jensherlevsen@gmail.com>
2020-10-27 10:12:13 +0100

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'promise-polyfill/src/polyfill'
-import * as serviceWorker from './serviceWorker'
 
 import App from './components/App'
-import './index.scss'
+import './global.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
