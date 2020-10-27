4dc613aa2dd325ea09500914122dbc033a75fca6
Jens Ahlsten Herlevsen <jensherlevsen@gmail.com>
2020-10-27 09:37:26 +0100

    update package versions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'promise-polyfill/src/polyfill'
+import * as serviceWorker from './serviceWorker'
+
+import App from './components/App'
+import './index.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
